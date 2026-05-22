d24493f4bd82786ba26cd4b0bcf653bbadbe8f1a
AmandhaYumi <mandhayumi@gmail.com>
2026-05-22 12:03:42 -0300

    feat: definir chaves de armazenamento local

---
+++ src/storage/chaves.js
@@ -0,0 +1,8 @@
+export const CHAVES = {
+  USUARIO: '@dobu:usuario',
+  USUARIOS: '@dobu:usuarios',
+  PETS: '@dobu:pets',
+  AGENDAMENTOS: '@dobu:agendamentos',
+  MONITORAMENTO: '@dobu:monitoramento',
+  PONTOS: '@dobu:pontos',
+};
